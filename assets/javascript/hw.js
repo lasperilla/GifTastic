f7e87ffe1d849cc15362234ed5a5298fd3f78902
lasperilla <l.asperilla@gmail.com>
2017-03-23 08:10:58 -0400

    wrote hw.js got starting buttons, simple ajax request, push images to page

---
+++ assets/javascript/hw.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+
+	var topics = ['All That', 'Kenan & Kel', 'Are You Afraid of the Dark?', 'Salute Your Shorts', 'Hey Dude', 'Legends of the Hidden Temple']
+
+	function makeButtonsFunc() {
+		for (var i = 0; i < topics.length; i++) {
+			var newButton = $('<button>')
+			newButton.addClass('categoryButton');
+			newButton.attr('data-topic', topics[i]);
+			newButton.text(topics[i]);
+			$('.gifButtons').append(newButton);
+		}
+	}
+
+	makeButtonsFunc();
+
+	$('.categoryButton').on('click', function() {
+		var searchTerm = $(this).attr('data-topic');
+		var apiKey = 'dc6zaTOxFJmzC';
+		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key="+apiKey+"&limit=10";
+
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+			})
+			.done(function(response) {
+				var results = response.data;
+				$('#categories').empty();
+
+				for (var i = 0; i < results.length; i++) {
+					if (results[i].rating !== "r") {
+						var gifDiv = $("<div class='item'>");
+						var rating = results[i].rating;
+						var p = $("<p>").text("Rating: " + rating);
+						var topicImage = $("<img>");
+						topicImage.attr("src", results[i].images.fixed_height.url);
+						gifDiv.append(p);
+						gifDiv.append(topicImage);
+
+						$('#categories').prepend(gifDiv);
+						console.log(queryURL);
+					}
+				}
+			});
+	}); //end of categoryButton on click
+
+}); //end document.ready
